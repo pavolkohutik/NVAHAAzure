7c4d63c68ab45703458c58d972b08ac9c2f3691b
pavolkohutik <51722105+pavolkohutik@users.noreply.github.com>
2022-03-10 12:38:38 +0000

    Update run.ps1

--- Function1/run.ps1
+++ Function1/run.ps1
@@ -50,8 +50,8 @@ Write-Output -InputObject "HA NVA timer trigger function executed at:$(Get-Date)
 
 $VMFW1Name = $env:FW1NAME      # Set the Name of the primary NVA firewall
 $VMFW2Name = $env:FW2NAME      # Set the Name of the secondary NVA firewall
-$FW1RGName = $env:FWRGNAME     # Set the ResourceGroup that contains FW1
-$FW2RGName = $env:FWRGNAME     # Set the ResourceGroup that contains FW2
+$FW1RGName = $env:FWRGNAME1     # Set the ResourceGroup that contains FW1
+$FW2RGName = $env:FWRGNAME2     # Set the ResourceGroup that contains FW2
 $Monitor = $env:FWMONITOR      # "VMStatus" or "TCPPort" are valid values
 
 #--------------------------------------------------------------------------
